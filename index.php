52bcb66e4518c24ebc21987ab23f816ac708069e
QuimArga <66687810+QuimArga@users.noreply.github.com>
2023-06-14 18:36:10 +0200

    inici

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+    <head>
+        <?php
+        include "incs/head.inc";
+        ?>   
+    </head>
+    <body>
+    <div class="container">
+      <div class="row">
+        <?php
+        include "incs/adlogistics_subirana.inc";
+        include "incs/adlogistics_mercafruits.inc";
+        include "incs/adlogistics_bruned.inc";
+        include "incs/adlogistics_joprimsa.inc";
+        include "incs/adcrm_balart.inc";
+        include "incs/adcrm_mercafruits.inc";
+        include "incs/adcrm_fradera.inc";
+        include "incs/adcrm_bruned.inc";
+        include "incs/adpalet_escandell.inc";
+        ?>
+        
+        
+      </div>   
+
+	
+    <?php
+        include "incs/footer.inc";
+    ?>    
+    </body>
+</html>
